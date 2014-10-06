4a7c8bfc99fcbe4b4793fbcf283724fc7294f4af
Alexander Gudulin <alexandr.gudulin@gmail.com>
2014-10-07 01:52:54 +0400

    Simplify samples presentation

--- js/3.js
+++ js/3.js
@@ -1,63 +1,78 @@
 $(document).ready(function(){
   console.log("loaded");
 
+  window.s = new Sample();
+});
+
+var Sample = function() {
   var $headings = $(".main h3");
 
-  // ----- #1
-  // console.log($headings);
+  return {
+    ex1: function() {
+      console.log($headings);
+    },
 
-  // ----- #2
-  // $headings.hide();
+    ex2: function() {
+      $headings.hide();
+    },
 
-  // ----- #3
-  // $headings.css({
-  //   color: "green"
-  // });
+    ex3: function() {
+      $headings.css({
+        color: "green"
+      });
+    },
 
-  // ----- #4
-  // $headings.on("click", function(){
-  //   alert("heading has been clicked");
-  // });
+    ex4: function() {
+      $headings.on("click", function(){
+        alert("heading has been clicked");
+      });
+    },
 
-  // ----- #5
-  // $headings.on("mouseenter", function(){
-  //   $(this).toggleClass("heading-active");
-  // });
+    ex5: function() {
+      $headings.on("mouseenter", function(){
+        $(this).toggleClass("heading-active");
+      });
+    },
 
-  // ----- #6
-  // $headings.on("click", function(){
-  //   $(this).next("p").slideToggle();
-  // });
+    ex6: function() {
+      $headings.on("click", function(){
+        $(this).next("p").slideToggle();
+      });
+    },
 
-  // ----- #7
-  // $xkcdComicBlock = $(".xkcd-comic");
-  // $xkcdComicBlock.on("click", function() {
-  //   $this = $(this);
-  //   // clean inner html
-  //   $this.find(".comic").html("");
-  //   // hide the element
-  //   $this.hide();
-  // });
-  // $("#lessons-btn").on("click", function() {
-  //   $.ajax({
-  //     url: "http://dynamic.xkcd.com/api-0/jsonp/comic?callback=?",
-  //     dataType: "json",
-  //
-  //     success: function(data) {
-  //       $xkcdComicBlock.find(".comic").append(
-  //         $("<img>").attr({
-  //           src: data.img
-  //         })
-  //       );
-  //     },
-  //
-  //     error: function() {
-  //       $xkcdComicBlock.find(".comic").text("Can't load comic :(");
-  //     },
-  //
-  //     complete: function() {
-  //       $xkcdComicBlock.show();
-  //     }
-  //   });
-  // });
-});
+    ex7: function() {
+      $xkcdComicBlock = $(".xkcd-comic");
+
+      $xkcdComicBlock.on("click", function() {
+        $this = $(this);
+        // clean inner html
+        $this.find(".comic").html("");
+        // hide the element
+        $this.hide();
+      });
+
+      $("#lessons-btn").on("click", function() {
+        $.ajax({
+          url: "http://dynamic.xkcd.com/api-0/jsonp/comic?callback=?",
+          dataType: "json",
+
+          success: function(data) {
+            $xkcdComicBlock.find(".comic").append(
+              $("<img>").attr({
+                src: data.img
+              })
+            );
+          },
+
+          error: function() {
+            $xkcdComicBlock.find(".comic").text("Can't load comic :(");
+          },
+
+          complete: function() {
+            $xkcdComicBlock.show();
+          }
+        });
+      });
+    }
+  }
+}
